0bebcaf39aab06e3db5fb9c3c1f565641bdfe443
Guido Bartolucci <guido@bartolucci.org>
2018-09-12 15:22:12 -0400

    Roll finally works

--- postscript.js
+++ postscript.js
@@ -13,7 +13,6 @@ let code = {
     dup: () => o.push(o[o.length-1]),
     copy: () => { let n = o.pop(); o = o.concat(o.slice(o.length-n)) },
     index: () => { let n = o.pop(); o.push(o[o.length - n - 1]) },
-    // TODO: roll https://stackoverflow.com/q/15997536/279104
     clear: () => o.length = 0,
     count: () => o.push(o.length),
     
@@ -29,6 +28,16 @@ let code = {
     closepath: () => c.closePath(),
 };
 
+code['roll'] = function() {
+    let j = o.pop()
+    let n = o.pop()
+    console.log(`${j} and ${n}`)
+    let s = o.splice(o.length-n)
+    if (j > 0) for (j; j>0; j--) s.unshift(s.pop())
+    if (j < 0) for (j; j<0; j++) s.push(s.shift())
+    o = o.concat(s)
+}
+
 function call(e) {
     console.log(`Calling ${e.sourceString}`)
     if (code[e.sourceString])
@@ -41,45 +50,49 @@ let semantics = parser.createSemantics()
 semantics.addOperation('eval', {
     Exp: e => e.eval(),
     PriExp: e => e.eval(),
-    PriExp_neg: (sign, e) => -1 * e.eval(),
+    PriExp_neg: (sign, e) => -e.eval(),
     word: (left, right) => left.eval() + right.eval(),
-    operator: e => call(e),
     number_fract: (a, b, c) => parseFloat(a.sourceString + '.' + c.sourceString),
     number_whole: e => parseInt(e.sourceString),
     string: (left, e, right) => e.sourceString,
 })
 
-function confirm(unparsed, stack) {
-    v = semantics(parser.match(unparsed)).eval()
-    assert.deepEqual(v, stack)
+function run(unparsed, stack) {
+    o.length = 0
+    //console.log(hexdump(unparsed))
+    let m = parser.match(unparsed)
+    if (m.succeeded()) {
+	semantics(m).run()
+	console.log(o)
+	assert.deepEqual(o, stack)
+	return
+    }
+    console.log(m)
 }
 
-confirm('33', [33])
-confirm('-33', [-33])
-confirm('32.1', [32.1])
-confirm('-33.1', [-33.1])
-confirm('0 -33.1', [0, -33.1])
+function push(a) {
+    console.log(`Pushing ${a}`)
+    return o.push(a)
+}
 
 semantics.addOperation('run', {
-    number: e => o.push(e.eval()),
-    string: (a, b, c) => o.push(b.sourceString),
+    PriExp_neg: (sign, e) => push(-e.eval()),
+    number: e => push(e.eval()),
+    string: (a, b, c) => push(b.sourceString),
     operator: e => call(e),
 })
 
-function run(unparsed, stack) {
-    o.length = 0
-    semantics(parser.match(unparsed)).run()
-    console.log(o)
-    assert.deepEqual(o, stack)
-}
+run('(a) (b) (c) 3 1 roll', ['c', 'a', 'b'])
+run('(a) (b) (c) 3 0 roll', ['a', 'b', 'c'])
+run('(a) (b) (c) 3 -1 roll', ['b', 'c', 'a'])
 
 run('1 2 3 4 pop', [1, 2, 3])
 run('1 2 3 4 exch', [1, 2, 4, 3])
 run('1 2 3 4 dup', [1, 2, 3, 4, 4])
 run('1 2 3 4 3 copy', [1, 2, 3, 4, 2, 3, 4])
-
 run('(a) (b) (c) (d) 0 index', ['a', 'b', 'c', 'd', 'd'])
 run('(a) (b) (c) (d) 3 index', ['a', 'b', 'c', 'd', 'a'])
-
 run('1 2 3 4 clear', [])
 run('1 1 1 1 count', [1, 1, 1, 1, 4])
+
+
